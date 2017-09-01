9eecea54ba691265e09e1406e089a27155be463d
Nicholas Mertin <nmertin@mertin.home>
2017-09-01 19:44:36 -0400

    Updated port configuration

--- src/drive.c
+++ src/drive.c
@@ -1,7 +1,7 @@
 void setDrive(sbyte left, sbyte right)
 {
-	gMotor[driveL1].power = gMotor[driveL2].power = gMotor[driveL3].power = left;
-	gMotor[driveR1].power = gMotor[driveR2].power = gMotor[driveR3].power = right;
+	gMotor[driveL1].power = gMotor[driveL2].power = left;
+	gMotor[driveR1].power = gMotor[driveR2].power = right;
 }
 
 void handleDrive()
