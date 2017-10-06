21f6f8dba409d69073f1855753bb525f6f5b6620
Nicholas Mertin <nmertin@mertin.home>
2017-10-06 18:27:31 -0400

    Updated task priority test

--- test/TaskPriorityTest.c
+++ test/TaskPriorityTest.c
@@ -1,5 +1,15 @@
+task test2()
+{
+}
+
 task test()
 {
+	sleep(1000);
+	writeDebugStreamLine("%d", getTaskPriority(test2));
+	startTask(test2);
+	sleep(100);
+	writeDebugStreamLine("%d", getTaskPriority(test2));
+	stopAllTasks();
 	while(true);
 }
 
@@ -9,5 +19,6 @@ task main()
 	startTask(test);
 	sleep(100);
 	writeDebugStreamLine("%d", getTaskPriority(test));
+	sleep(2000);
 	stopAllTasks();
 }
