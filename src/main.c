28a7fd3c6b83e35de69a5d6048f569d2ed2ea738
Nicholas Mertin <nmertin@mertin.home>
2017-10-06 20:53:02 -0400

    Renamed ultrasonic

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 #pragma config(Sensor, dgtl5,  driveEncL,      sensorQuadEncoder)
 #pragma config(Sensor, dgtl7,  driveEncR,      sensorQuadEncoder)
 #pragma config(Sensor, dgtl9,  latEnc,         sensorQuadEncoder)
-#pragma config(Sensor, dgtl11, clawSonic,      sensorSONAR_mm)
+#pragma config(Sensor, dgtl11, frontSonic,     sensorSONAR_mm)
 #pragma config(Motor,  port1,           arm,           tmotorVex393_HBridge, openLoop, reversed)
 #pragma config(Motor,  port2,           liftL1,        tmotorVex393HighSpeed_MC29, openLoop, reversed)
 #pragma config(Motor,  port3,           driveL1,       tmotorVex393HighSpeed_MC29, openLoop)
