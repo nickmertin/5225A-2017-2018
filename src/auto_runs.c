98edfd175e087cc7e31023b688110afc0bd36413
anjaleenaren <31371146+anjaleenaren@users.noreply.github.com>
2017-11-30 22:29:40 -0500

    Tested code - issues w/ encoders and moveToTarget
     The right encoder is on backwards. Also, the moveToTarget does not work b/c the virtual worlds bot doesn't have a middle encoder.

--- src/auto_runs.c
+++ src/auto_runs.c
@@ -1,5 +1,8 @@
 void runAuto()
 {
-	resetPositionFull(gPosition, 19.5, 44.5, 45);
-	moveToTarget(50, 50, 90, 3, 1, 1.5, true, true);
+	//resetPositionFull(gPosition, 19.5, 44.5, 45);
+	//moveToTarget(50, 50, 90, 3, 1, 1.5, true, true);
+	setDrive(127, 127);
+	sleep(1000);
+	setDrive(0, 0);
 }
