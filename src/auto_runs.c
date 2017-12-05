64dc1e144da06c96906421d77e0e000cb401415a
anjaleenaren <31371146+anjaleenaren@users.noreply.github.com>
2017-12-04 20:59:05 -0500

    Tested encoders

--- src/auto_runs.c
+++ src/auto_runs.c
@@ -1,8 +1,8 @@
 void runAuto()
 {
-	resetPositionFull(gPosition, 19.5, 44.5, 45);
-	moveToTarget(50, 50, 90, 3, 1, 1.5, true, true);
-	//setDrive(127, 127);
-	sleep(10000);
+	//resetPositionFull(gPosition, 19.5, 44.5, 45);
+	//moveToTarget(50, 50, 19.5, 44.5,127, 2 1, 1.5, true, true);
+	setDrive(127,127)
+	while (gSensor[leftEnc].value < 300) sleep(10);
 	setDrive(0, 0);
 }
