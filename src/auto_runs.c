1e0306883d52a3d3086b603c277fd249ad5e38aa
Nick Mertin <nickmertin@gmail.com>
2017-12-11 09:48:38 -0500

    Fixed syntax issue

--- src/auto_runs.c
+++ src/auto_runs.c
@@ -2,7 +2,7 @@ void runAuto()
 {
 	//resetPositionFull(gPosition, 19.5, 44.5, 45);
 	//moveToTarget(50, 50, 19.5, 44.5,127, 2 1, 1.5, true, true);
-	setDrive(127,127)
+	setDrive(127,127);
 	while (gSensor[leftEnc].value < 300) sleep(10);
 	setDrive(0, 0);
 }
