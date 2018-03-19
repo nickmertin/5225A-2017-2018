66d16a864985901dbd6011636b5abc82be18ca3b
Nick Mertin <nickmertin@gmail.com>
2018-03-19 19:29:14 -0400

    Fixed issues in moveToTargetSimple algorithm

--- src/auto_simple.c
+++ src/auto_simple.c
@@ -71,13 +71,13 @@ void moveToTargetSimple(float y, float x, float ys, float xs, byte power, byte s
 			case mttCascading:
 				const float kB = 2.8;
 				const float kP = 1.4;
-				float vTarget = 45 * (1 - exp(currentPosVector.y + dropEarly));
+				float vTarget = 45 * (1 - exp(0.1 * (currentPosVector.y + dropEarly)));
 				finalPower = round(kB * vTarget + kP * (vTarget - vel)) * sgn(power);
 				break;
 			}
 			LIM_TO_VAL_SET(finalPower, abs(power));
-			if (finalPower * sgn(power) < abs(decelPower))
-				finalPower = decelPower;
+			if (finalPower * sgn(power) < 30)
+				finalPower = 30 * sgn(power);
 			word delta = finalPower - last;
 			LIM_TO_VAL_SET(delta, 5);
 			finalPower = last += delta;
