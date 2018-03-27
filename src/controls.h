426176725860e2631fc17e311b2adec48c3b2919
anjaleenaren <31371146+anjaleenaren@users.noreply.github.com>
2018-03-26 22:08:57 -0400

    Revert "Changed controls so they are different for driver and skills"
    This reverts commit 5025666aa7e28b9dc63f31de7f8712b4056c8f95.

--- src/controls.h
+++ src/controls.h
@@ -24,14 +24,11 @@
 #define BTN_MACRO_STACK Btn8D
 #define BTN_MACRO_LOADER Btn7D
 #define BTN_MACRO_PREP Btn7R
-#define BTN_MACRO_WALL Btn7L
-#define BTN_MACRO_STATIONARY Btn8R
+//#define BTN_MACRO_WALL Btn7L
+#define BTN_MACRO_STATIONARY (gSensor[jmpSkills].value ? Btn7D : Btn8R)
 #define BTN_MACRO_PRELOAD (gSensor[jmpSkills].value ? Btn8R : Btn7D)
-#define BTN_MACRO_PICKUP (gSensor[jmpSkills].value ? Btn7L : Btn8L)
+#define BTN_MACRO_PICKUP Btn8L
 #define BTN_MACRO_CANCEL Btn8U
 #define BTN_MACRO_INC Btn5U
 #define BTN_MACRO_DEC Btn5D
-
-//Skills only Macros
-#define BTN_MACRO_TILT Btn8L
-#define BTN_MACRO_STACK_NO_PU Btn8R
+#define BTN_MACRO_TILT Btn7L
