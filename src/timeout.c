1bf7c0cd7838b2fca6e75d679b9dec909357c82f
anjaleenaren <31371146+anjaleenaren@users.noreply.github.com>
2018-04-22 18:04:58 -0400

    Modified timeout libraries to take velSafetyCounter as an arg

--- src/timeout.c
+++ src/timeout.c
@@ -10,7 +10,7 @@ void timeoutWhileNotEqual(void *val1, void *val2, unsigned int size, unsigned lo
 	writeDebugStreamLine("Time left %d [%s - %d]", timeout - nPgmTime, routine, id);
 }
 
-void timeoutWhileLessThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign)
+void timeoutWhileLessThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign, int& velSafetyCounter)
 {
 	if (correctSign)
 		vel = abs(vel);
@@ -21,36 +21,36 @@ void timeoutWhileLessThanL(tTimeoutVelSourceType velSourceType, unsigned long ve
 	writeDebugStreamLine("Time left %d [%s - %d]", timeout - nPgmTime, routine, id);
 }
 
-void timeoutWhileGreaterThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign)
+void timeoutWhileGreaterThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign, int& velSafetyCounter)
 {
 	if (correctSign)
 		vel = -abs(vel);
 	if (velSourceType == velSensor)
 		velocityClear(velSourceData);
 	unsigned long startTime = nPgmTime;
-	while (*val > threshold && !TimedOut(timeout, routine, id, kill, velSourceType, velSourceData, vel, nPgmTime-startTime)) sleep(10);
+	while (*val > threshold && !TimedOut(timeout, routine, id, kill, velSourceType, velSourceData, vel, nPgmTime-startTime, velSafetyCounter)) sleep(10);
 	writeDebugStreamLine("Time left %d [%s - %d]", timeout - nPgmTime, routine, id);
 }
 
-void timeoutWhileLessThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign)
+void timeoutWhileLessThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign, int& velSafetyCounter)
 {
 	if (correctSign)
 		vel = abs(vel);
 	if (velSourceType == velSensor)
 		velocityClear(velSourceData);
 	unsigned long startTime = nPgmTime;
-	while (*val < threshold && !TimedOut(timeout, routine, id, kill, velSourceType, velSourceData, vel, nPgmTime-startTime)) sleep(10);
+	while (*val < threshold && !TimedOut(timeout, routine, id, kill, velSourceType, velSourceData, vel, nPgmTime-startTime, velSafetyCounter)) sleep(10);
 	writeDebugStreamLine("Time left %d [%s - %d]", timeout - nPgmTime, routine, id);
 }
 
-void timeoutWhileGreaterThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign)
+void timeoutWhileGreaterThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill, bool correctSign, int& velSafetyCounter)
 {
 	if (correctSign)
 		vel = -abs(vel);
 	if (velSourceType == velSensor)
 		velocityClear(velSourceData);
 	unsigned long startTime = nPgmTime;
-	while (*val > threshold && !TimedOut(timeout, routine, id, kill, velSourceType, velSourceData, vel, nPgmTime-startTime)) sleep(10);
+	while (*val > threshold && !TimedOut(timeout, routine, id, kill, velSourceType, velSourceData, vel, nPgmTime-startTime, velSafetyCounter)) sleep(10);
 	writeDebugStreamLine("Time left %d [%s - %d]", timeout - nPgmTime, routine, id);
 }
 
--- src/timeout.h
+++ src/timeout.h
@@ -18,9 +18,9 @@ typedef enum _timeoutVelSourceType
 /* Functions */
 void timeoutWhileEqual(void *val1, void *val2, unsigned int size, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true);
 void timeoutWhileNotEqual(void *val1, void *val2, unsigned int size, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true);
-void timeoutWhileLessThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true);
-void timeoutWhileGreaterThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true);
-void timeoutWhileLessThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true);
-void timeoutWhileGreaterThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true);
+void timeoutWhileLessThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true, int& velSafetyCounter);
+void timeoutWhileGreaterThanL(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, long *val, long threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true, int& velSafetyCounter);
+void timeoutWhileLessThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true,  int& velSafetyCounter);
+void timeoutWhileGreaterThanF(tTimeoutVelSourceType velSourceType, unsigned long velSourceData, float vel, float *val, float threshold, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true, bool correctSign = true, int& velSafetyCounter);
 void timeoutWhileTrue(bool *val, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true);
 void timeoutWhileFalse(bool *val, unsigned long timeout, const unsigned char *routine, unsigned short id, bool kill = true);
